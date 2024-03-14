a66818fe24a18ad30115ad4611efdd141d8c8c62
Harshitha Shetty <harshitha_shetty@thbs.india.com>
2024-03-14 15:41:47 +0530

    Enable Dark mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import TextForm from './components/TextForm';
 import './App.css';
 import Navbar from './components/Navbar';
+import { useState } from 'react';
 function App() {
+  const [mode, setMode]= useState('light');
+  
+  const togglemode=()=>{
+    if(mode==='dark'){
+      setMode ('light')
+      document.body.style.backgroundColor='white';
+    }else {
+      setMode('dark')
+      document.body.style.backgroundColor='#032643';
+    }
+
+  }
   return (
     <>
 
-   <Navbar Title="TextUtils"/>
+   <Navbar Title="TextUtils" mode={mode} togglemode={togglemode}/>
    <div className="container my-3">
-   <TextForm heading="Enter Text"/>
+   <TextForm heading="Enter Text" mode={mode}/>
    </div>
         
   </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,25 +1,48 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
+  const [text, setText] =useState('Enter text here');
     const handleUpclick = () =>{
-        console.log("Uppercase" + text);
+        //console.log("Uppercase" + text);
         let newText=text.toUpperCase();
         setText(newText)
 
     }
+    const handleLowclick = () =>{
+      //console.log("Uppercase" + text);
+      let newText=text.toLowerCase();
+      setText(newText)
+
+  }
     const handleChange = (event) =>{
-        console.log("Onchange");
+        //console.log("Onchange");
         setText(event.target.value)
     }
     
-    const [text, setText] =useState('Enter text here');
+    const handleemail = () =>{
+      const email = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+      let newText2=text.match(email) || [];
+        setText(newText2.join(", "))
+    }
+    
+   
   return (
-    <div>
+    <>
+    <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleChange} id="myBox" rows="8"></textarea>
-        <button className="btn btn-primary" onClick={handleUpclick} >ConvertToUppercase</button>
-        </div>
+        <textarea className="form-control my-3" value={text} onChange={handleChange} id="myBox" rows="8" style={{backgroundColor:props.mode==='dark'?'grey':'white'}} ></textarea>
+        <button className="btn btn-primary mx-2" onClick={handleUpclick} >ConvertToUppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLowclick} >ConvertToLowerercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleemail} >ExtracEmailAddress</button>
+      </div>
     </div>
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
+      <h1> Your text summary</h1>
+      <p>{text.split(" ").length} and {text.length}</p>
+      <h2>Preview</h2>
+      <p>{text.length>0?text:"Enter text to preview"}</p>
+    </div>
+    </>
   )
 }
